f2d83a79cc2094de9119483634d82643396d22b5
olena <olenapatsevko@gmail.com>
2021-04-19 01:37:53 +0300

    refactoring

---
+++ src/main/java/tef/smarth/service/impl/CalculationServiceImpl.java
@@ -0,0 +1,12 @@
+package tef.smarth.service.impl;
+
+import tef.smarth.dto.Record;
+import tef.smarth.entity.Calculation;
+import tef.smarth.service.CalculationService;
+
+public class CalculationServiceImpl implements CalculationService {
+    @Override
+    public Calculation caluclateRitis(Record ast, Record alt) {
+        return null;
+    }
+}
