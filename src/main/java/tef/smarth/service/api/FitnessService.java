e6b717eec480d00ad243151872e53227db2a64da
olena <olenapatsevko@gmail.com>
2021-04-30 17:47:53 +0300

    refactoring

--- src/main/java/tef/smarth/service/api/FitnessService.java
+++ src/main/java/tef/smarth/service/api/FitnessService.java
@@ -1,4 +1,9 @@
 package tef.smarth.service.api;
 
+import tef.smarth.api.fitness.response.FitnessResponse;
+import tef.smarth.entity.UserEntity;
+
 public interface FitnessService {
+
+    FitnessResponse getFitnessPlan(UserEntity userEntity);
 }
--- src/main/java/tef/smarth/service/api/impl/FitnessServiceImpl.java
+++ src/main/java/tef/smarth/service/api/impl/FitnessServiceImpl.java
@@ -1,6 +1,15 @@
 package tef.smarth.service.api.impl;
 
+import org.springframework.stereotype.Service;
+import tef.smarth.api.fitness.response.FitnessResponse;
+import tef.smarth.entity.UserEntity;
 import tef.smarth.service.api.FitnessService;
 
+@Service
 public class FitnessServiceImpl implements FitnessService {
+
+
+   public FitnessResponse getFitnessPlan(UserEntity userEntity){
+        return null;
+    }
 }
